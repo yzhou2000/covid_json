1f3c76dce370ae285a3291538f5a2843a9efc528
YuZhou <yzhou2000@outlook.com>
2020-04-25 13:21:30 -0500

    April 25 change

---
+++ us_covid.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv'
+df = pd.read_csv(url)
+df=df[['date','county','state','cases','deaths']]
+df.to_json(r'./us_covid.json' , orient = 'records')
+
+

4ffd2520c078b00f5370f1b823c14bb3d15b6467
YuZhou <yzhou2000@outlook.com>
2020-04-26 11:14:24 -0500

    Test commit

--- us_covid.py
+++ us_covid.py
@@ -4,5 +4,5 @@ df = pd.read_csv(url)
 df=df[['date','county','state','cases','deaths']]
 df.to_json(r'./us_covid.json' , orient = 'records',lines=True)
 
-tn_df=df[[df['state' == 'Tennessee']]]
-df.to_json(r'./tn.json' , orient = 'records',lines=True)
+tndf=df[df['state'] == 'Tennessee']
+tndf.to_json(r'./tn.json' , orient = 'records',lines=True)

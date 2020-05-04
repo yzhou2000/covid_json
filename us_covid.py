930fe4c40618f074d47ea9627b345ffef2480b29
YuZhou <yzhou2000@outlook.com>
2020-05-04 09:39:19 -0500

    Test commit

--- us_covid.py
+++ us_covid.py
@@ -5,6 +5,9 @@ df = pd.read_csv(url)
 #getting uszip data
 uszip = pd.read_csv('/home/yuzhou/AndroidStudioProjects/covid_json/uszip.csv')
 
+uszipcounty=uszip[['county','state','ZIP']].dropna()
+us_zip.to_json('./us_zip.json' , orient = 'records')
+
 uscounty=uszip[['county','state','msa']].dropna()
 
 #create us county msa list
